7668cb5b4853e5c09ddad69c193f67484b77776d
Naga Sai Vamsi Chilakalapudi <80962513+vamsi2106@users.noreply.github.com>
2025-04-30 15:55:15 +0530

    first commit

---
+++ src/components/Blog/blogData.tsx
@@ -0,0 +1,47 @@
+import { Blog } from "@/types/blog";
+
+const blogData: Blog[] = [
+  {
+    id: 1,
+    title: "Face Value of the Securities. Face Value of the Securities",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+    image: "/images/blog/blog-01.jpg",
+    author: {
+      name: "Samuyl Joshi",
+      image: "/images/blog/author-01.png",
+      designation: "Forex Trader",
+    },
+    tags: ["creative"],
+    publishDate: "2025",
+  },
+  {
+    id: 2,
+    title: "Eligibility Criteria to Get an Bank Security (SBLC & BG)",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+    image: "/images/blog/blog-02.jpg",
+    author: {
+      name: "Musharof Chy",
+      image: "/images/blog/author-02.png",
+      designation: "Business Analyst",
+    },
+    tags: ["computer"],
+    publishDate: "2025",
+  },
+  {
+    id: 3,
+    title: "Transaction Procedure - For DLC Issuance.",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+    image: "/images/blog/blog-03.jpg",
+    author: {
+      name: "Lethium Deo",
+      image: "/images/blog/author-03.png",
+      designation: "Lithium Diode",
+    },
+    tags: ["design"],
+    publishDate: "2025",
+  },
+];
+export default blogData;
---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,116 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "About",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 33,
+    title: "Blog",
+    path: "/blog",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Support",
+    path: "/contact",
+    newTab: false,
+  },
+  {
+    id: 4,
+    title: "Services",
+    newTab: false,
+    submenu: [
+      {
+        id: 41,
+        title: "Bank Securities",
+        path: "/contact",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Corporate Finance",
+        path: "/contact",
+        newTab: false,
+      },
+      {
+        id: 43,
+        title: "Investment Banking",
+        path: "/contact",
+        newTab: false,
+      },
+      {
+        id: 44,
+        title: "Financial Advisory",
+        path: "/contact",
+        newTab: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Overview",
+    newTab: false,
+    submenu: [
+      {
+        id: 41,
+        title: "About Page",
+        path: "/about",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Contact Page",
+        path: "/contact",
+        newTab: false,
+      },
+      {
+        id: 43,
+        title: "Blog Grid Page",
+        path: "/blog",
+        newTab: false,
+      },
+      {
+        id: 44,
+        title: "Blog Sidebar Page",
+        path: "/blog-sidebar",
+        newTab: false,
+      },
+      {
+        id: 45,
+        title: "Blog Details Page",
+        path: "/blog-details",
+        newTab: false,
+      },
+      {
+        id: 46,
+        title: "Sign In Page",
+        path: "/signin",
+        newTab: false,
+      },
+      {
+        id: 47,
+        title: "Sign Up Page",
+        path: "/signup",
+        newTab: false,
+      },
+      {
+        id: 48,
+        title: "Error Page",
+        path: "/error",
+        newTab: false,
+      },
+    ],
+  },
+  
+];
+export default menuData;
